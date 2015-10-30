f7f200fd876873014c35ab1de429efa715dce0d2
Francid <splinzy@yahoo.com>
2015-10-29 23:46:02 -0400

    Change layouts and added the java files

---
+++ app/src/main/java/com/example/franchez/lopvent/EventCategories.java
@@ -0,0 +1,13 @@
+package com.example.franchez.lopvent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EventCategories extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_categories);
+    }
+}
---
+++ app/src/main/java/com/example/franchez/lopvent/SavedList.java
@@ -0,0 +1,13 @@
+package com.example.franchez.lopvent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SavedList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saved_list);
+    }
+}
