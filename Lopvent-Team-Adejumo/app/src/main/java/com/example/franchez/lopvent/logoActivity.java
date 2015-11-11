11a43e3e04a743421215dbd152861f91883e0b06
singhAvneet <avneet_cochhar@hotmail.com>
2015-11-11 18:34:33 -0500

    tested resources in drawble folder

---
+++ Lopvent-Team-Adejumo/app/src/main/java/com/example/franchez/lopvent/logoActivity.java
@@ -0,0 +1,17 @@
+package com.example.franchez.lopvent;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+
+/**
+ * Created by Owner on 2015-11-11.
+ */
+public class logoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
